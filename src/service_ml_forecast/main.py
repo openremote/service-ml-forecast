1704e601d513e7b02d7110440a6d0b9adb220295
Dominique Kleeven <10584854+dominiquekleeven@users.noreply.github.com>
2025-03-07 17:49:31 +0100

    Wip

--- src/service_ml_forecast/main.py
+++ src/service_ml_forecast/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from service_ml_forecast.system.status_resource import get_status_router
-from service_ml_forecast import __app_info__
+from service_ml_forecast import AppInfo, __app_info__
 import logging
 
 logger = logging.getLogger(__name__)
 
 def create_app() -> FastAPI:
+
+    if __app_info__ is None:
+        logger.error("App initialization failed: Failed to read app info")
+        raise RuntimeError("App initialization failed: Failed to read app info")
+
     app = FastAPI(
         title=__app_info__.name,
         description=__app_info__.description,
