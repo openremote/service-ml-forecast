e9f09591d816eea4bf182b5eaef7fdbcb1f9e688
Dominique Kleeven <10584854+dominiquekleeven@users.noreply.github.com>
2025-03-26 15:00:26 +0100

    Review fixes - added copyright headers, fix type warnings
    - Added license headers
    - Fix unused param in main lifespan
    - Specify config.logging to satisfy import
    - Specify return type for OpenRemoteClient fixtures

--- src/service_ml_forecast/main.py
+++ src/service_ml_forecast/main.py
@@ -1,4 +1,21 @@
-import logging
+# Copyright 2025, OpenRemote Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+import logging.config
 from collections.abc import AsyncGenerator
 
 import uvicorn
@@ -22,7 +39,7 @@ if __app_info__ is None:
 
 # FastAPI Lifecycle, handles startup and shutdown tasks
 @asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+async def lifespan(_app: FastAPI) -> AsyncGenerator[None, None]:
     # Startup tasks
     logger.info("Starting application")
     logger.info("Application details: %s", __app_info__)
