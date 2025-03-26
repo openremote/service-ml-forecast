4f406352657958fd54c0adb349db6733dcf9f675
Dominique Kleeven <10584854+dominiquekleeven@users.noreply.github.com>
2025-03-26 15:11:32 +0100

    Add license headers

--- src/service_ml_forecast/services/forecast_service.py
+++ src/service_ml_forecast/services/forecast_service.py
@@ -1,7 +1,16 @@
-# Forecast Service
+# Copyright 2025, OpenRemote Inc.
 #
-# This service is responsible for generating forecasts. It does this by using the
-# trained model and model config.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
-# The forecast values are written to the OpenRemote API.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
 #
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
--- src/service_ml_forecast/services/training_service.py
+++ src/service_ml_forecast/services/training_service.py
@@ -1,11 +1,19 @@
-# Training Service
+# Copyright 2025, OpenRemote Inc.
 #
-# This service is responsible for training the model. The training data is read
-# from the OpenRemote API based on the model config. The training is done on a
-# schedule based on the model config.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
-# The trained model is then stored in the local file system.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
 #
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
 
 
 class TrainingService:
