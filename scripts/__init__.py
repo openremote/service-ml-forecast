e29ba2604d32e25c0f801ec71272cc18c13a4b4a
Dominique Kleeven <10584854+dominiquekleeven@users.noreply.github.com>
2025-04-04 16:45:29 +0200

    Migrate to `uv` project and package manager (#23)
    * Migrate to uv package and project manager
    * Update README.md
    * Enforce python version via .python-version file
    * Quick command rename serve -> start

---
+++ scripts/__init__.py
@@ -0,0 +1,7 @@
+"""
+Helper scripts for various tasks such as linting, formatting, testing, etc.
+"""
+
+from scripts.tools import build, format, lint, start, test, test_coverage
+
+__all__ = ["build", "format", "lint", "start", "test", "test_coverage"]
