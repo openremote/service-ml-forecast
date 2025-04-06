d3f35abb2d48e20bfb5a20c72b1b3109d8953ab6
Dominique Kleeven <10584854+dominiquekleeven@users.noreply.github.com>
2025-04-06 13:14:06 +0200

    License headers

--- src/service_ml_forecast/api/route_exception_handlers.py
+++ src/service_ml_forecast/api/route_exception_handlers.py
@@ -1,8 +1,22 @@
-"""
-This module contains the exception handlers for the FastAPI application.
+# Copyright 2025, OpenRemote Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
 
-These need to be defined and registered with the FastAPI application otherwise
-FastAPI will throw 500 errors to the client.
+"""
+Exception handlers for the FastAPI application.
 """
 
 from http.client import CONFLICT, NOT_FOUND
